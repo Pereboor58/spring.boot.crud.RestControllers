ff10f086f2b2dbe210a56a0839854df65a61cbe7
Pereboor58 <121056185+Pereboor58@users.noreply.github.com>
2023-06-02 10:01:49 +0300

    Add files via upload

---
+++ spring_boot_rest/src/main/java/com/example/spring_boot_rest/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.example.spring_boot_rest.dao;
+
+import com.example.spring_boot_rest.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    List<Employee> findAllByName(String name);
+}
---
+++ spring_boot_rest/src/main/java/com/example/spring_boot_rest/service/EmployeeServiceImpl.java
@@ -0,0 +1,50 @@
+package com.example.spring_boot_rest.service;
+
+import com.example.spring_boot_rest.dao.EmployeeRepository;
+import com.example.spring_boot_rest.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+// @Service означает бизнес-логику. Она соединяет controller и dao
+// При использовании JpaRepository аннотации Transactional не нужны
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+        employeeRepository.save(employee);
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
+        Employee employee = null;
+        Optional<Employee> optional = employeeRepository.findById(id);
+        if (optional.isPresent()) {
+            employee = optional.get();
+        }
+        return employee;
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+        employeeRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Employee> findAllByName(String name) {
+        List<Employee> employees = employeeRepository.findAllByName(name);
+        return employees;
+    }
+}
